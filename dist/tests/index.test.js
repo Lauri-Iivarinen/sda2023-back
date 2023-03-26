561911554ad118cf1907b7ffca0c8df0d5a8a1af
Lauri <94760484+Lauri-Iivarinen@users.noreply.github.com>
2023-03-26 02:33:36 +0200

    Delete dist directory

--- dist/tests/index.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const globals_1 = require("@jest/globals");
-const node_assert_1 = require("node:assert");
-(0, globals_1.test)('Tests are working', () => {
-    node_assert_1.strict.ok('ok');
-});
