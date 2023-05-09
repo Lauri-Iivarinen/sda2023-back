c2718726a943814a8152a952279175ec76d9c15e
Lauri Iivarinen <lauri.iivarinen@gmail.com>
2023-05-09 16:38:57 +0300

    added cors but couldnt use with modules

--- index.ts
+++ index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import sqlite3 from "sqlite3";
 
+const cors = require('cors')
+
 const app = express()
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({
     limit: '5mb',
     extended: true
